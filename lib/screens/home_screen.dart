a9b6e749d4ba2b746270b6c6719170a28b8d55a1
jebril.foitih <foitihj@gmail.com>
2026-03-23 22:00:50 +0100

    Fin de session : Navigation OK, Menu OK, README mis a jour

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
+import '../models/product.dart';
+import '../services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final ApiService apiService = ApiService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Appbar(title: const Text ("Mon Restaurant")),
-      body: const Center(child: Text("Bienvenue en salle !")),
+      appBar: AppBar(
+        title: const Text("Le menu du Resto"),
+        backgroundColor: Colors.orange,
+      ),
+      body: FutureBuilder<List<Product>>(
+        future: apiService.getProducts(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Erreur : ${snapshot.error}"));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text("Aucun produit trouvé."));
+          } else {
+            final products = snapshot.data!;
+            return ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final product = products[index];
+                return ListTile(
+                  leading: Image.network(
+                    product.imageUrl, 
+                    width: 50, 
+                    height: 50, 
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) => const Icon(Icons.broken_image),
+                  ),
+                  title: Text(product.title),
+                  subtitle: Text("${product.price} €"),
+                  trailing: const Icon(Icons.add_shopping_cart, color: Colors.orange),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
